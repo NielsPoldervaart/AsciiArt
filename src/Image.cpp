b1159bf09f44b540b10fc13e7090491572bfdecb
Niels Poldervaart <nielspoldervaart@gmail.com>
2026-03-29 01:42:51 +0100

    Add external stb_image lib

---
+++ src/Image.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Niels Poldervaart on 22/03/2026.
+//
+
+#include "Image.h"
---
+++ src/Image.h
@@ -0,0 +1,8 @@
+//
+// Created by Niels Poldervaart on 22/03/2026.
+//
+
+#ifndef ASCIIART_IMAGE_H
+#define ASCIIART_IMAGE_H
+
+#endif //ASCIIART_IMAGE_H
